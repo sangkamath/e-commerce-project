48b60095b1caeeb3b31cdade6cbe70b11d99836c
sangkamath <twinkle_kamath@yahoo.com>
2025-04-14 12:27:46 -0700

    Added button variants and test, including setup

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,24 +1,50 @@
-import React from "react";
- import { cva, type VariantProps } from "class-variance-authority";
- import { cn } from "@/lib/utils";
- 
- const buttonVariants = cva("bg-white", {
-   variants: {
-     variant: {
-       default: "bg-red-500",
-       primary: "bg-blue-500",
-       secondary: "bg-green-500",
-     },
-     size: {
-       default: "p-4",
-       small: "p-2",
-       large: "p-6",
-     },
-   },
-   defaultVariants: {
-     variant: "default",
-     size: "default",
-   },
- });
- 
- export { buttonVariants };
+import React, { ButtonHTMLAttributes } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from  "@/lib/utils";
+
+const buttonVariants = cva(
+  "rounded font-medium transition-colors duration-300 ease-in-out",
+  {
+    variants: {
+      variant: {
+        primary: "bg-red-500",
+        secondary: "bg-blue-500",
+        tertiary: "bg-green-500 leading-10",
+        destructive: "",
+        linkGray: "",
+        linkColor: "",
+      },
+      size: {
+        medium: "inline-flex h-10  items-center  gap-1 px-[14px] text-[14px]",
+        large: "inline-flex h-11 items-center gap-1.5 px-4 text-base",
+        extraLarge: "inline-flex h-12 items-center gap-1.5 px-5 text-base",
+        large2XL: "inline-flex h-[60px] items-center gap-2.5 px-6 text-[18px]",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "medium",
+    },
+  },
+);
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={cn(buttonVariants({ variant, size, className }))}
+    >
+      {children}
+    </button>
+  );
+};
+export { buttonVariants, Button };
