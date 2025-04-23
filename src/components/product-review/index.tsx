fa86c6734f40648c30358a639207484e2b4ac6c1
sangkamath <twinkle_kamath@yahoo.com>
2025-04-23 15:14:34 -0700

    Added progress, rating statistics to review, and load more pagination for reviews

--- src/components/product-review/index.tsx
+++ src/components/product-review/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { DialogDescription, DialogTitle } from "@/components/ui/dialog";
-import { useQuery } from "@tanstack/react-query";
 import { cn } from "@/lib/utils";
 import { getProductReview } from "@/lib/services";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Rating } from "@mui/material";
 import { StarIcon } from "lucide-react";
 import { Button } from "../button";
-import { formatDate, toBase64, getInitials } from "@/lib/helper";
-import { Shimmer } from "../icons";
+import { getInitials, getRatingText } from "@/lib/helper";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Progress } from "@/components/ui/progress";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
 
 export default function ProductReview({ productName }: { productName: string }) {
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, isError, fetchNextPage, hasNextPage } = useInfiniteQuery({
         queryKey: ["reviews", productName],
-        queryFn: async () => await getProductReview(productName),
+        queryFn: async ({ pageParam = 1 }) => await getProductReview("voyager-hoodie", { page: pageParam }),
+        getNextPageParam: (lastPage, pages) =>
+            lastPage.pagination.has_more ? lastPage.pagination.page + 1 : undefined,
+        initialPageParam: 1,
     });
 
     if (isLoading) {
@@ -31,6 +35,21 @@ export default function ProductReview({ productName }: { productName: string }) 
 
     }
 
+    if (isError) {
+        return <>
+            <DialogTitle asChild>
+                <VisuallyHidden>
+                    <h2>My Accessible Title</h2>
+                </VisuallyHidden>
+            </DialogTitle>
+            <DialogDescription className={cn("mt-6 pt-6")}>
+                Error loading reviews
+            </DialogDescription>
+        </>
+    }
+
+    const allReviews = data?.pages.flatMap((page) => page.data) ?? [];
+
     return (
         <>
             <DialogTitle asChild>
@@ -40,10 +59,14 @@ export default function ProductReview({ productName }: { productName: string }) 
             </DialogTitle>
             <DialogDescription>
             </DialogDescription>
-            <div>
-                <div className="flex flex-col gap-10 lg:gap-8">
-                    <ReviewsSummary data={data.aggregate} />
-                    <Reviews data={data.data} />
+            <div className="h-full w-full xl:mt-1 xl:pt-1">
+                <div className="flex flex-col gap-10 lg:flex-row lg:gap-8">
+                    <ReviewsSummary data={data?.pages[0].aggregate} />
+                    <Reviews
+                        data={allReviews}
+                        fetchNextPage={fetchNextPage}
+                        hasNextPage={hasNextPage}
+                    />
                 </div>
             </div>
         </>
@@ -51,13 +74,32 @@ export default function ProductReview({ productName }: { productName: string }) 
 }
 
 function ReviewsSummary({ data }: { data: any }) {
+    const sortedCounts = [...data.counts].sort((a, b) => b.rating - a.rating);
+
+    function getRatingColor(rating: number): string {
+        switch (rating) {
+            case 5:
+                return "#16A34A";
+            case 4:
+                return "#22C55E";
+            case 3:
+                return "#FDE047";
+            case 2:
+                return "#EAB308";
+            case 1:
+                return "bg-red-500";
+            default:
+                return "bg-gray-200";
+        }
+    }
+
     return (
-        <div className="flex w-full max-w-[384px] flex-col gap-6">
+        <div className="flex w-full flex-col  xl:max-w-[384px]">
             <div className="flex flex-col gap-2">
                 <h2 className="text-xl font-semibold leading-7 text-neutral-900">
                     Overall Rating
                 </h2>
-                <div className="flex items-center gap-2">
+                <div className="mb-1 flex items-center gap-2">
                     <span className="text-base font-semibold leading-6 text-neutral-900">
                         {data?.rating ?? 0}
                     </span>
@@ -67,30 +109,47 @@ function ReviewsSummary({ data }: { data: any }) {
                         precision={0.5}
                         value={data?.rating}
                         emptyIcon={
-                            <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+                            <StarIcon style={{ opacity: 0.55 }} fontSize="h-4 w-4" />
                         }
                     />
                     {data?.total > 0 ? <p>{`Based on ${data.total} reviews`}</p> : null}
                 </div>
             </div>
-            <div>stat</div>
-            <Button variant="secondary" className={cn("mx-auto w-[153px] px-5 py-3")}>
+            <div className="flex w-full flex-col gap-4">
+                {sortedCounts.map((count: any, index: number) => (
+                    <div key={index} className="flex items-center gap-2">
+                        <p className="w-[120px] text-base font-medium leading-6 text-neutral-600">
+                            {getRatingText(count.rating)}
+                        </p>
+                        <Progress
+                            value={count.count}
+                            max={100}
+                            className={`h-2 flex-1 bg-gray-200`}
+                            color={getRatingColor(count.rating)}
+                        />
+                        <p className="text-right text-base font-normal leading-6 text-neutral-600">
+                            {count.count}
+                        </p>
+                    </div>
+                ))}
+            </div>
+            <Button variant="secondary" className={cn("mx-auto mt-4 w-[153px] px-5 py-3")}>
                 Write a review
             </Button>
         </div>
     )
 }
 
-function Reviews({ data }: { data: any }) {
+function Reviews({ data, fetchNextPage, hasNextPage }: { data: any[], fetchNextPage: any, hasNextPage: boolean }) {
     if (!data || !Array.isArray(data)) {
         return <div>No reviews available</div>;
     }
 
     return (
-        <>
-            <div className="flex w-full flex-col gap-6 lg:flex-1">
+            <div className="flex h-full flex-1 flex-col gap-1 md:gap-4">
+                <div className="flex h-[290px] flex-col gap-6 overflow-scroll md:h-[320px] xl:h-[420px]">
                 {data.map((review: any, index: number) => (
-                    <div className="flex flex-col gap-4" key={index}>
+                    <div className="flex h-full max-h-[112px] flex-col gap-4" key={index}>
                         <div className="flex gap-4">
                             <div className="relative h-12 w-12 overflow-hidden rounded-full">
                                 {review.user.avatar_url ? (
@@ -126,9 +185,15 @@ function Reviews({ data }: { data: any }) {
                     </div>
                 ))}
             </div>
-            <Button variant="secondary" className={cn("mx-auto w-full px-5 py-3")}>
-                Show 10 more reviews
-            </Button>
-        </>
+            {hasNextPage && (
+                <Button
+                    variant="secondary"
+                    className={cn("mx-auto mt-10 w-full px-5 py-3")}
+                    onClick={() => fetchNextPage()}
+                >
+                    Show 10 more reviews
+                </Button>
+            )}
+        </div>
     );
 }
